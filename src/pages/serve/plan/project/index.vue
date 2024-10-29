3e24e96f6ee88dbea04189b3998e697a943d9957
tensei <tennsei716@gmail.com>
2024-10-29 09:23:32 +0800

    feat(page/s/p/project): 初始化护理项目模块的UI结构

---
+++ src/pages/serve/plan/project/index.vue
@@ -0,0 +1,22 @@
+<!-- 护理项目列表页 -->
+<template>
+  <div class="min-h serveProject bg-wt">
+    <!-- 筛选区域 -->
+    <SearchFormBox></SearchFormBox>
+    <!-- end -->
+    <!-- 表格 -->
+    <TableList></TableList>
+    <!-- end -->
+    <!-- 新增，编辑弹窗 -->
+    <DialogFrom></DialogFrom>
+    <!-- end -->
+    <!-- 删除弹层 -->
+    <Delete></Delete>
+    <!-- end -->
+    <!-- 禁用弹层 -->
+    <Forbidden></Forbidden>
+    <!-- end -->
+  </div>
+</template>
+
+<script setup lang="ts"></script>
